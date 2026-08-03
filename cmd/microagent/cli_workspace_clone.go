288c0c3682afbfdfd1aa297b53d7f2a0422cfa28
Geoff Belknap <geoffbelknap@users.noreply.github.com>
2026-08-03 08:32:53 -0700

    Remove superseded clone helpers

--- cmd/microagent/cli_workspace_clone.go
+++ cmd/microagent/cli_workspace_clone.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/geoffbelknap/microagent/pkg/vmkit"
 	"github.com/geoffbelknap/microagent/pkg/workspace"
@@ -13,100 +9,6 @@ import (
 
 func cloneWorkspace(stateDir, source, target string) (workspaceResult, error) {
 	return workspace.Clone(stateDir, source, target)
-}
-
-func ensureWorkspaceCloneable(stateDir, name string) error {
-	state, err := readWorkspaceRuntimeState(workspaceOptions{StateDir: stateDir, Name: name})
-	if os.IsNotExist(err) {
-		event, eventErr := readWorkspaceEvent(workspaceOptions{StateDir: stateDir, Name: name})
-		if os.IsNotExist(eventErr) {
-			return nil
-		}
-		if eventErr != nil {
-			return eventErr
-		}
-		return cloneableState(name, event.State)
-	}
-	if err != nil {
-		event, eventErr := readWorkspaceEvent(workspaceOptions{StateDir: stateDir, Name: name})
-		if os.IsNotExist(eventErr) {
-			return err
-		}
-		if eventErr != nil {
-			return err
-		}
-		return cloneableState(name, event.State)
-	}
-	return cloneableState(name, state.Event.State)
-}
-
-func cloneableState(name string, state vmkit.VMState) error {
-	switch state {
-	case "", vmkit.StateUnknown, vmkit.StatePrepared, vmkit.StateHalted, vmkit.StateStopped:
-		return nil
-	default:
-		return fmt.Errorf("workspace %s must be stopped before cloning; current state is %s", name, state)
-	}
-}
-
-func rewriteClonedDiskPaths(disks []workspaceDisk, sourceWorkspaceDir, targetWorkspaceDir string) []workspaceDisk {
-	if len(disks) == 0 {
-		return nil
-	}
-	out := make([]workspaceDisk, 0, len(disks))
-	sourceWorkspaceDir = filepath.Clean(sourceWorkspaceDir)
-	for _, disk := range disks {
-		if rel, err := filepath.Rel(sourceWorkspaceDir, disk.Path); err == nil && rel != "." && !strings.HasPrefix(rel, ".."+string(os.PathSeparator)) && rel != ".." {
-			disk.Path = filepath.Join(targetWorkspaceDir, rel)
-		}
-		out = append(out, disk)
-	}
-	return out
-}
-
-func copyDirectory(source, target string) error {
-	return filepath.WalkDir(source, func(path string, entry os.DirEntry, walkErr error) error {
-		if walkErr != nil {
-			return walkErr
-		}
-		rel, err := filepath.Rel(source, path)
-		if err != nil {
-			return err
-		}
-		targetPath := filepath.Join(target, rel)
-		info, err := entry.Info()
-		if err != nil {
-			return err
-		}
-		if entry.IsDir() {
-			return os.MkdirAll(targetPath, info.Mode().Perm())
-		}
-		if info.Mode()&os.ModeType != 0 {
-			return fmt.Errorf("cannot clone special file %s", path)
-		}
-		return copyFile(path, targetPath, info.Mode().Perm())
-	})
-}
-
-func copyFile(source, target string, mode os.FileMode) error {
-	in, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-	defer func() { _ = in.Close() }()
-	if err := os.MkdirAll(filepath.Dir(target), 0o755); err != nil {
-		return err
-	}
-	out, err := os.OpenFile(target, os.O_CREATE|os.O_EXCL|os.O_WRONLY, mode)
-	if err != nil {
-		return err
-	}
-	_, copyErr := io.Copy(out, in)
-	closeErr := out.Close()
-	if copyErr != nil {
-		return copyErr
-	}
-	return closeErr
 }
 
 // validateWorkspaceName is the CLI adapter over the library's one name rule;
